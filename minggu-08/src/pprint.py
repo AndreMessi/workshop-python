22b84fc7c5c1949e657823963435765eca50d92a
andreputra <hninja049@gmail.com>
2019-05-12 08:50:58 -0400

    adding minggu-08

---
+++ minggu-08/src/pprint.py
@@ -0,0 +1,6 @@
+import pprint
+
+t = [[[['black', 'cyan'], 'white', ['green', 'red']], [['magenta',
+  'yellow'], 'blue']]]
+
+pprint.pprint(t, width=30)
---
+++ minggu-08/src/texwrap.py
@@ -0,0 +1,6 @@
+import textwrap
+doc = """The wrap() method is just like fill() except that it returns
+    a list of strings instead of one big string with newlines to separate
+    the wrapped lines."""
+
+print(textwrap.fill(doc, width=40))
